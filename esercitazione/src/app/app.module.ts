084df580bc8a3db64c9feeaee66720256e148edc
Marco Bonnì <marco.bonni@accenture.com>
2023-06-22 15:51:13 +0200

    first

--- esercitazione/src/app/app.module.ts
+++ esercitazione/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserSingleComponent } from './user-single/user-single.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserContainerComponent } from './user-container/user-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserSingleComponent,
+    UserDetailComponent,
+    UserContainerComponent
   ],
   imports: [
     BrowserModule,
