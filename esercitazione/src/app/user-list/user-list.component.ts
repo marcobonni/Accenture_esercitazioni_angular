2c074969546e0c5dcaa2cc6a77174000509fe450
Marco Bonnì <marco.bonni@accenture.com>
2023-06-28 16:13:46 +0200

    first try

--- esercitazione/src/app/user-list/user-list.component.ts
+++ esercitazione/src/app/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+ 
 
 @Component({
   selector: 'app-user-list',
@@ -6,7 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
-
+  user = [
+    {
+        id: 3487,
+        name: 'Mario',
+        surname: 'Rossi',
+        age: 25,
+        dateOfBirth: '1995-14-12',
+        address: {
+          city: 'Roma',
+          street: 'Via roma 10',
+          postalCode: '00100'
+        },
+        role: "STAFF",
+        username: 'MarioRossi80',
+        profilePhotoUrl: 'https://bit.ly/3yRngEP',
+        gender: "MEAL"
+      },
+      {
+        id: 12312,
+        name: 'Maria',
+        surname: 'Rossa',
+        age: 26,
+        dateOfBirth: '1994-10-11',
+        address: {
+          city: 'Palermo',
+          street: 'Via Palermo 10',
+          postalCode: '90100'
+        },
+        role: "MANAGER",
+        username: 'MariaRossa94',
+        profilePhotoUrl: 'https://bit.ly/3DWWxuj',
+        gender: "FEMALE"
+      },
+      {
+        id: 45645,
+        name: 'Elis',
+        surname: 'Miao',
+        age: 5,
+        dateOfBirth: '2016-05-08',
+        address: {
+          city: 'Palermo',
+          street: 'Via Palermo 120',
+          postalCode: '90100'
+        },
+        role: "ADMIN",
+        username: 'ElisMiao',
+        profilePhotoUrl: 'https://bit.ly/3zU6iH3',
+        gender: "FEMALE"
+      }
+  ];
   constructor() { }
 
   ngOnInit(): void {
