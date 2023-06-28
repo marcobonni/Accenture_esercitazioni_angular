faa6a09260e6014912d8af6214e84ff6289e01d1
marcobonni <marco.bonni19@gmail.com>
2023-06-28 11:46:08 +0200

    first

--- esercitazione/src/app/user-detail/user-detail.component.ts
+++ esercitazione/src/app/user-detail/user-detail.component.ts
@@ -1,19 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 
+interface isAddress {
+  city: string;
+  street: string;
+  postalCode: string;
+}
+interface isCompany {
+  name: string;
+  description: string;
+}
+interface isUserInfo {
+  id: number;
+  name : string;
+  surname : string;
+  age : number;
+  dateOfBirth : string;
+  address : isAddress;
+  role : "staff" | "admin";
+  username : string;
+  img_url:  string;
+  gender : "male" | "female" | "other";
+  company : isCompany;
+}
+
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.scss']
 })
 
-interface isUserInfo {
-  id: string;
-
-}
 export class UserDetailComponent implements OnInit {
-   user_info : any  = {
-    id : 
-  }
+  user : isUserInfo  = {
+    id : 3487,
+    name: "Mario",
+    surname: "Rossi",
+    age: 30,
+    dateOfBirth : "1995-Dec-14",
+    address :  {
+      city : "Roma",
+      street : "via Roma 10",
+      postalCode : "00100",
+    },
+    role: "staff",
+    username: "Super",
+    img_url: "https://picsum.photos/id/237/536/354",
+    gender : "male",
+    company : {
+      name : "Develhope",
+      description :  "La migliore"
+    }
+    }
+  
   constructor() { }
 
   ngOnInit(): void {
